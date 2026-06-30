e46ee89170ab9aeb545099161641d4df55de6fa9
Williane Gonçalves <wgs14@discente.ifpe.edu.br>
2026-06-30 16:52:08 -0300

    Implement contatoController with CRUD operations

---
+++ controllers/contatoController.js
@@ -0,0 +1,68 @@
+const Contato = require('../models/Contato')
+
+// Listar todos os contatos
+const listar = async (req, res) => {
+  const contatos = await Contato.findAll({ raw: true })
+  res.render('contatos/index', { contatos })
+}
+
+// Mostrar formulário de novo contato
+const novoContato = (req, res) => {
+  res.render('contatos/novo')
+}
+
+// Criar contato
+const criar = async (req, res) => {
+  const { campus, telefone, email } = req.body
+
+  await Contato.create({
+    campus,
+    telefone,
+    email
+  })
+
+  res.redirect('/contatos')
+}
+
+// Mostrar formulário de edição
+const editar = async (req, res) => {
+  const contato = await Contato.findByPk(req.params.id)
+
+  if (!contato) {
+    return res.redirect('/contatos')
+  }
+
+  res.render('contatos/editar', {
+    contato: contato.toJSON()
+  })
+}
+
+// Atualizar contato
+const atualizar = async (req, res) => {
+  const { campus, telefone, email } = req.body
+
+  await Contato.update(
+    { campus, telefone, email },
+    { where: { id: req.params.id } }
+  )
+
+  res.redirect('/contatos')
+}
+
+// Excluir contato
+const deletar = async (req, res) => {
+  await Contato.destroy({
+    where: { id: req.params.id }
+  })
+
+  res.redirect('/contatos')
+}
+
+module.exports = {
+  listar,
+  novoContato,
+  criar,
+  editar,
+  atualizar,
+  deletar
+}
